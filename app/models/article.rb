7842633f0e26fe82ff8daba113bbe92e91c2aefb
Daisuke Harada <dharada@guidewire.com>
2017-12-10 16:57:16 +0900

    update just as following tutorial

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ApplicationRecord
-  validates :title, presence: true, length: { minimum: 5 }
+  # If you delete an article, its associated comments will also need to be deleted, otherwise they would simply occupy space in the database.
+  has_many :comments, dependent: :destroy
+  validates :title, presence: true, length: {minimum: 5}
 end
